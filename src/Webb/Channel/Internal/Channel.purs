509ab4dccc19d316b8a9f86dfd3b49d15b46002c
Howard Chen <hchen7913@gmail.com>
2025-12-29 23:44:59 -0600

    Add stubs for the send and receive functions

--- src/Webb/Channel/Internal/Channel.purs
+++ src/Webb/Channel/Internal/Channel.purs
@@ -11,14 +11,27 @@ import Webb.Channel.Internal.State (CState)
 {- Channel internal functions.
 -}
 
-type Channel a = CState a
+type Channel = CState
 
 -- Sending a value may hang, because if the buffer is full, we
 -- have to wait, in order, for the send to complete.
-send :: forall a. Channel a -> a -> Aff Unit
-send chan a = pure unit
+send :: forall a. Channel -> a -> Aff Unit
+send chan a = do
+  s <- Sender.new
+  success <- Sender.send s
+  
+  -- Do we succeed in sending immediately?
+  unless success do
+    Sender.wait s
 
 -- Receiving a value may hang, because if no value is present to
 -- take, then we have to wait for a value.
-receive :: forall a. Channel a -> Aff (Maybe a)
-receive chan = pure Nothing
+receive :: forall a. Channel -> Aff (Maybe a)
+receive chan = do 
+  r <- Receiver.new
+  mvalue <- Receiver.receive r
+  case mvalue of
+    Just value -> do
+      pure $ Just value
+    Nothing -> do
+      Receiver.wait r
